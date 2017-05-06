e2960bf0fa29672bfeb92a141af73794d1e35593
Gary Wong <gary.wonghg@gmail.com>
2017-05-06 16:07:43 -0700

    (init) Add JobInfo component

--- client/src/components/JobInfo.jsx
+++ client/src/components/JobInfo.jsx
@@ -1,15 +1,34 @@
 import React, { PropTypes, Component } from 'react';
-import { FlatButton, RaisedButton, Dialog } from 'material-ui/Dialog';
+import Dialog from 'material-ui/Dialog';
 
 const propTypes = {
-
+  jobPost: PropTypes.object.isRequired,
 };
 
-const customContentStyle = {
-  width: '100%',
-  maxWidth: 'none',
-};
-
-export default class JobEntry extends Component {
-
+export default JobInfo = (jobPost) => {
+  const { comapny,
+          job_title,
+          job_description,
+          basic_qualifications,
+          preferred_qualifications,
+          location,
+          posted_date,
+          posted_url,
+        } = jobPost;
+  return (
+    <div>
+      <h1>Title</h1>
+      { job_title } at { company }
+      <h1>Location</h1>
+      { location }
+      <h1>Description</h1>
+      { job_description }
+      <h1>Basic Qualifications</h1>
+      { basic_qualifications }
+      <h1>Preferred Qualifications</h1>
+      { preferred_qualifications }
+      <h1>Job Posting Url</h1>
+      { post_url }
+    </div>
+  )
 }
