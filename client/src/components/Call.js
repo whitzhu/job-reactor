822efaddb82cdc7d62101beb75858a55bde7608f
Brandon Wong <b.lh.wong@gmail.com>
2017-05-06 22:10:25 -0700

    Create render for asking questions and hearing question

--- client/src/components/Call.js
+++ client/src/components/Call.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export default class Call extends React.Component {
   static propTypes = {
@@ -7,13 +9,52 @@ export default class Call extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      prompt: 0,
+      question: Math.floor(Math.random() * 3)
+    };
   }
 
   render() {
-    return (
-      <div>
-        <h1>Call</h1>
-      </div>
-    );
+    console.log(this.state);
+    if (this.state.prompt === 1) {
+      return (
+        <div>
+          <audio autoPlay='true' src={this.state.question + '.mp3'} controls></audio>
+          <MuiThemeProvider>
+            <RaisedButton
+              label="Click here to record answer"
+              onTouchTap={() => {
+                let prompt = this.state.prompt;
+                this.setState({
+                  prompt: prompt+1
+                });
+              }}
+
+            />
+          </MuiThemeProvider>
+
+        </div>
+
+      );
+    } else {
+      return (
+        <div>
+          <h1>Ready for your question?</h1>
+          <MuiThemeProvider>
+            <RaisedButton
+              label="Click here to begin"
+              onTouchTap={() => {
+                let prompt = this.state.prompt;
+                this.setState({
+                  prompt: prompt+1
+                });
+              }}
+            />
+          </MuiThemeProvider>
+        </div>
+
+      );
+    }
   }
 }
