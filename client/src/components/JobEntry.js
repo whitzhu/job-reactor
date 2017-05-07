e87599944b1d7ce7e4a64ac70767cf6bc5a9a1c1
Gary Wong <gary.wonghg@gmail.com>
2017-05-06 21:12:25 -0700

    (feat) Add jobEntry form

--- client/src/components/JobEntry.js
+++ client/src/components/JobEntry.js
@@ -1,28 +1,48 @@
 import React, { PropTypes, Component } from 'react';
 import { FlatButton, RaisedButton, Dialog } from 'material-ui/Dialog';
+import TextField from 'material-ui/TextField';
 
-const propTypes = {
+// const propTypes = {
 
-};
+// };
 
-const customContentStyle = {
-  width: '100%',
-  maxWidth: 'none',
-};
+// const customContentStyle = {
+//   width: '100%',
+//   maxWidth: 'none',
+// };
 
 export default class JobEntry extends Component {
-  static propTypes = {
+  // static propTypes = {
 
-  }
+  // }
 
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }  
 
   render() {
     return (
       <div>
-        <h1>Job entry component</h1>
+        <TextField
+          hintText="Hint Text"
+          errorText="This field is required"
+        /><br />
+        <TextField
+          hintText="Hint Text"
+          errorText="The error text can be as long as you want, it will wrap."
+        /><br />
+        <TextField
+          hintText="Hint Text"
+          errorText="This field is required"
+          floatingLabelText="Floating Label Text"
+        /><br />
+        <TextField
+          hintText="Message Field"
+          errorText="This field is required."
+          floatingLabelText="MultiLine and FloatingLabel"
+          multiLine={true}
+          rows={2}
+        /><br />
       </div>
     )
   }  
