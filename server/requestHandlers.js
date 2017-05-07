eccb45f9628d667859ecd92cec5920741bffd0bb
Gary Wong <gary.wonghg@gmail.com>
2017-05-07 10:12:52 -0700

    (feat) Add request handler storeJobPosting function

--- server/requestHandlers.js
+++ server/requestHandlers.js
@@ -1,1 +1,8 @@
 const user = require('../database/models/users.js');
+const dbh = require('../database/db_helpers');
+
+module.exports.storeJobPosting = (req, res) => {
+  const details = req.body;
+  console.log('what are the details??123', details);
+  return dbh.
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('express-cors')
 const app = express()
 
 const db = require('../database/db_config.js');
+const rh = require('./requestHandlers');
 
 app.use(cors({
   allowedOrigins: ['https://site@site.com/']
@@ -15,6 +16,8 @@ app.use(express.static(__dirname + '/../client/dist'))
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/../client/dist/index.html'))
 })
+
+app.post('/storeJobPosting', rh.storeJobPosting);
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(`Server is listening on PORT ${process.env.port || 3000}`)
