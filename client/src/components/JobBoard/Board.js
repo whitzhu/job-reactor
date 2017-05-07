43a9d6f41e4e0856aa2393b87d87aec50459f437
whitzhu <whitzhu@gmail.com>
2017-05-06 21:52:07 -0700

    [Job Board] update Board to render ListCardContainer based on state

--- client/src/components/JobBoard/Board.js
+++ client/src/components/JobBoard/Board.js
@@ -9,46 +9,59 @@ export default class Board extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      list: [
+        { 
+          header: "Interested",
+          cards: [
+            { id: 1, company: "Item 1", title: "software engineer" },
+            { id: 2, company: "Item 2", title: "software engineer" },
+            { id: 3, company: "Item 3", title: "software engineer" }
+          ]
+        },
+        {
+          header: "Applied",
+          cards: [
+            { id: 4, company: "Item 4", title: "software engineer"},
+            { id: 5, company: "Item 5", title: "software engineer" },
+            { id: 6, company: "Item 6", title: "software engineer" }
+          ]
+        },
+        {
+
+          header: "Phone",
+          cards: [
+            { id: 7, company: "Item 7", title: "software engineer" },
+            { id: 8, company: "Item 8", title: "software engineer" },
+            { id: 9, company: "Item 9", title: "software engineer" }
+          ]
+        },
+        {
+          header: "On-Site",
+          cards: [
+            { id: 10, company: "Item 7", title: "software engineer" },
+            { id: 11, company: "Item 8", title: "software engineer" },
+            { id: 12, company: "Item 9", title: "software engineer" }
+          ]
+        },
+        {
+          header: "Offer",
+          cards: [
+            { id: 13, company: "Item 7", title: "software engineer" },
+            { id: 14, company: "Item 8", title: "software engineer" },
+            { id: 15, company: "Item 9", title: "software engineer" }
+          ]
+        }
+      ]
+    }
   }
 
-  render() {
-    const listOne = [
-      { id: 1, company: "Item 1", title: "software engineer" },
-      { id: 2, company: "Item 2", title: "software engineer" },
-      { id: 3, company: "Item 3", title: "software engineer" }
-    ];
- 
-    const listTwo = [
-      { id: 4, company: "Item 4", title: "software engineer"},
-      { id: 5, company: "Item 5", title: "software engineer" },
-      { id: 6, company: "Item 6", title: "software engineer" }
-    ];
- 
-    const listThree = [
-      { id: 7, company: "Item 7", title: "software engineer" },
-      { id: 8, company: "Item 8", title: "software engineer" },
-      { id: 9, company: "Item 9", title: "software engineer" }
-    ];    
-
-    const listFour = [
-      { id: 10, company: "Item 7", title: "software engineer" },
-      { id: 11, company: "Item 8", title: "software engineer" },
-      { id: 12, company: "Item 9", title: "software engineer" }
-    ];
-
-    const listFive = [
-      { id: 13, company: "Item 7", title: "software engineer" },
-      { id: 14, company: "Item 8", title: "software engineer" },
-      { id: 15, company: "Item 9", title: "software engineer" }
-    ];
-
+  render() {    
     return (
       <div className="job-board">
-        <ListCardContainer id={1} list={listOne} header="Interested"/>
-        <ListCardContainer id={2} list={listTwo} header="Applied"/>
-        <ListCardContainer id={3} list={listThree} header="Phone Interview"/>
-        <ListCardContainer id={3} list={listFour} header="On-Site"/>
-        <ListCardContainer id={3} list={listFive} header="Offer"/>
+      {this.state.list.map( (listcontainer, i) => (
+        <ListCardContainer id={i} list={listcontainer.cards} header={listcontainer.header}/>
+      ))}
       </div>
     );
   }
