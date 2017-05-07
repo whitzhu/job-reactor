d4a56e2bbc67abc86ec03e37dbca31fdc71fc480
whitzhu <whitzhu@gmail.com>
2017-05-06 21:55:37 -0700

    update dummy data

--- client/src/components/JobBoard/Board.js
+++ client/src/components/JobBoard/Board.js
@@ -14,42 +14,42 @@ export default class Board extends React.Component {
         { 
           header: "Interested",
           cards: [
-            { id: 1, company: "Item 1", title: "software engineer" },
-            { id: 2, company: "Item 2", title: "software engineer" },
-            { id: 3, company: "Item 3", title: "software engineer" }
+            { id: 1, company: "Angel List", title: "software engineer" },
+            { id: 2, company: "Airbnb", title: "software engineer" },
+            { id: 3, company: "Apple", title: "software engineer" }
           ]
         },
         {
           header: "Applied",
           cards: [
-            { id: 4, company: "Item 4", title: "software engineer"},
-            { id: 5, company: "Item 5", title: "software engineer" },
-            { id: 6, company: "Item 6", title: "software engineer" }
+            { id: 4, company: "Tesla", title: "software engineer"},
+            { id: 5, company: "Microsoft", title: "software engineer" },
+            { id: 6, company: "Lyft", title: "software engineer" }
           ]
         },
         {
 
           header: "Phone",
           cards: [
-            { id: 7, company: "Item 7", title: "software engineer" },
-            { id: 8, company: "Item 8", title: "software engineer" },
-            { id: 9, company: "Item 9", title: "software engineer" }
+            { id: 7, company: "Angel Hack", title: "software engineer" },
+            { id: 8, company: "SparkPost", title: "software engineer" },
+            { id: 9, company: "Radix", title: "software engineer" }
           ]
         },
         {
           header: "On-Site",
           cards: [
-            { id: 10, company: "Item 7", title: "software engineer" },
-            { id: 11, company: "Item 8", title: "software engineer" },
-            { id: 12, company: "Item 9", title: "software engineer" }
+            { id: 10, company: "Amazon", title: "software engineer" },
+            { id: 11, company: "Philz", title: "software engineer" },
+            { id: 12, company: "Blue Bottle", title: "software engineer" }
           ]
         },
         {
           header: "Offer",
           cards: [
-            { id: 13, company: "Item 7", title: "software engineer" },
-            { id: 14, company: "Item 8", title: "software engineer" },
-            { id: 15, company: "Item 9", title: "software engineer" }
+            { id: 13, company: "Yelp", title: "software engineer" },
+            { id: 14, company: "Twitter", title: "software engineer" },
+            { id: 15, company: "IBM", title: "software engineer" }
           ]
         }
       ]
@@ -59,8 +59,12 @@ export default class Board extends React.Component {
   render() {    
     return (
       <div className="job-board">
-      {this.state.list.map( (listcontainer, i) => (
-        <ListCardContainer id={i} list={listcontainer.cards} header={listcontainer.header}/>
+      {this.state.list.map( listcontainer => (
+        <ListCardContainer
+          key={Math.floor(Math.random()*100)} 
+          id={Math.floor(Math.random()*100)} 
+          list={listcontainer.cards} 
+          header={listcontainer.header}/>
       ))}
       </div>
     );
